0455c718d6b92bab9714e270e9dd498d3b176472
Gregory Priznar <44954435+Gpriznar@users.noreply.github.com>
2019-07-08 14:56:47 -0500

    Added Logic - Site 90% Finished

--- didtheastroswintoday/src/components/TodaysScore.js
+++ didtheastroswintoday/src/components/TodaysScore.js
@@ -2,34 +2,48 @@ import React, {Component} from 'react'
 import axios from 'axios'
 import './TodaysScore.css';
 
+
+
 export class TodaysScore extends Component {
   constructor() {
     super()
 
     this.state = {
-      games: []
+      games: [],
+      offday: true
     }
   }
 
-
-  WinCheck = () => {
-    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.sportradar.us/mlb-t6/games/2019/07/07/boxscore.json?api_key=82zanc4f93d4xd6x96awntpw`,{crossdomain:true})
+componentDidMount = () => {
+    let todaysDate = new Date();
+    console.log(todaysDate)
+    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.sportradar.us/mlb-t6/games/${todaysDate.getFullYear()}/${(todaysDate.getMonth()+1)}/${todaysDate.getUTCDate()}/boxscore.json?api_key=82zanc4f93d4xd6x96awntpw`,{crossdomain:true})
     .then(response => {
       console.log(response)
-      this.setState({games: response.data.league.games})
-    })
+      if (response.data.league.games === undefined){
+        this.setState({offday: true})
+      } else {
+        response.data.league.games.map((game) => {
+       if( game.game.home_team === 'eb21dadd-8f10-4095-8bf3-dfb3b779f107' || game.game.away_team === 'eb21dadd-8f10-4095-8bf3-dfb3b779f107'){
+          this.setState({games: response.data.league.games, offday: false})
+        }
+      }
+    )
   }
-
+ })
+}
   render(){
 
-    const gameinfo = this.state.games.map((info, index) => {
-      if (index === 1 || index === 2 || index === 3 || index === 4 || index === 5 || index === 6 || index === 7 || index === 8 || index === 9 || index === 10 || index === 11 || index === 12 || index === 13 || index === 14 || index === 15)
 
+    const gameinfo = this.state.games.map((info, index) => {
+
+    if (info.game.home_team === 'eb21dadd-8f10-4095-8bf3-dfb3b779f107' && info.game.home.runs > info.game.away.runs)
 
     {return(
       <div>
       <ul className="ScoresList">
       <li>
+      <p> You know it! </p>
       <p> {info.game.home.name} - {info.game.home.runs} </p>
       <p> {info.game.away.name} - {info.game.away.runs} </p>
       </li>
@@ -37,15 +51,38 @@ export class TodaysScore extends Component {
       </div>
       )
     }
-  })
+    else if (info.game.home_team === 'eb21dadd-8f10-4095-8bf3-dfb3b779f107' && info.game.home.runs < info.game.away.runs)
+        {return(
+        <div>
+        <ul className="ScoresList">
+        <li>
+        <p> Not Today Champ </p>
+        <p> {info.game.home.name} - {info.game.home.runs} </p>
+        <p> {info.game.away.name} - {info.game.away.runs} </p>
+        </li>
+        </ul>
+        </div>
+        )
+      }
+    })
 
-    return (
-      <div className="WinButton">
-      <button onClick={this.WinCheck}>Did they win?</button>
-      {gameinfo}
-      </div>
+    if(this.state.offday === true){
+      return(
+        <div>
+        < p className="noGame"> No game so far today! Check back later</p>
+        </div>
     )
   }
+  else {
+    return (
+      <div className="WinButton">
+        {gameinfo}
+      </div>
+    )
+   }
+  }
+
+
 }
 
 export default TodaysScore
