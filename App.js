e8a5881abf0291927a5c2c0bc2b6bae7f40f5d95
Tim Goens <46302093+TimothyCGoens@users.noreply.github.com>
2019-07-10 13:30:51 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TodaysScore from './components/TodaysScore'
+import './App.css';
+
+function App() {
+  return (
+    <div className='mainpage'>
+    <h1 className='DTAWT'> Did the Astros Win Today? </h1>
+    <div className="bar-zero"></div>
+    <div className="bar-one">1</div>
+    <div className="bar-two">2</div>
+    <div className="bar-three">2</div>
+    <div className="bar-four">2</div>
+    <div className="bar-five"></div>
+    <div className="bar-six">1</div>
+    <div className="bar-seven">2</div>
+    <div className="bar-eight">2</div>
+    <div className="bar-nine">2</div>
+    <div className="score-display"> </div>
+    
+    </div>
+  );
+}
+
+export default App;
