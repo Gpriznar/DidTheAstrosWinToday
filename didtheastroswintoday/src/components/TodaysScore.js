62f6f3f7efff8d3e6b5e9ba47d849e956b3a3f4d
Benjamin Juarez <ben95juarez@gmail.com>
2019-07-10 11:37:11 -0500

    lil refactoring action

--- didtheastroswintoday/src/components/TodaysScore.js
+++ didtheastroswintoday/src/components/TodaysScore.js
@@ -1,88 +1,88 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import {apiKey} from '../apiKey'
 import './TodaysScore.css';
+import Schedule from './Schedule/Schedule'
 
 
 
 export class TodaysScore extends Component {
   constructor() {
+    let today = new Date()
     super()
-
     this.state = {
-      games: [],
-      offday: true
+      teamId: 'eb21dadd-8f10-4095-8bf3-dfb3b779f107',
+      today: today,
+      todaysGame: null,
+      myTeamAtHome: null
     }
   }
 
-componentDidMount = () => {
-    let todaysDate = new Date();
-    console.log(todaysDate)
-    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.sportradar.us/mlb-t6/games/${todaysDate.getFullYear()}/${(todaysDate.getMonth()+1)}/${todaysDate.getUTCDate()}/boxscore.json?api_key=82zanc4f93d4xd6x96awntpw`,{crossdomain:true})
+  componentDidMount = () => {
+    let today = this.state.today 
+    let teamId = this.state.teamId
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.sportradar.us/mlb-t6/games/${today.getFullYear()}/${(today.getMonth()+1)}/${today.getUTCDate()}/boxscore.json?api_key=${apiKey}`,{crossdomain:true})
     .then(response => {
-      console.log(response)
-      if (response.data.league.games === undefined){
-        this.setState({offday: true})
-      } else {
-        response.data.league.games.map((game) => {
-       if( game.game.home_team === 'eb21dadd-8f10-4095-8bf3-dfb3b779f107' || game.game.away_team === 'eb21dadd-8f10-4095-8bf3-dfb3b779f107'){
-          this.setState({games: response.data.league.games, offday: false})
-        }
-      }
-    )
+      let games = response.data.league.games
+      let todaysGame = games ? games.filter(game => {
+        return (game.game.home_team === teamId || game.game.away_team === teamId)
+      })[0] : null
+      this.setState({
+        todaysGame: todaysGame, 
+        myTeamAtHome: todaysGame ? todaysGame.home_team === teamId : null
+      })
+    })
+    .catch(error => console.log(error))
   }
- })
-}
+
   render(){
-
-
-    const gameinfo = this.state.games.map((info, index) => {
-
-    if (info.game.home_team === 'eb21dadd-8f10-4095-8bf3-dfb3b779f107' && info.game.home.runs > info.game.away.runs)
-
-    {return(
-      <div>
-      <ul className="ScoresList">
-      <li>
-      <p> You know it! </p>
-      <p> {info.game.home.name} - {info.game.home.runs} </p>
-      <p> {info.game.away.name} - {info.game.away.runs} </p>
-      </li>
-      </ul>
-      </div>
-      )
+    let body = null
+    if(!this.state.game){
+      body = ( <div><p className="noGame"> No game so far today! Check back later</p></div> )
     }
-    else if (info.game.home_team === 'eb21dadd-8f10-4095-8bf3-dfb3b779f107' && info.game.home.runs < info.game.away.runs)
-        {return(
-        <div>
-        <ul className="ScoresList">
-        <li>
-        <p> Not Today Champ </p>
-        <p> {info.game.home.name} - {info.game.home.runs} </p>
-        <p> {info.game.away.name} - {info.game.away.runs} </p>
-        </li>
-        </ul>
-        </div>
+    else {
+      let myTeamAtHome = this.state.myTeamAtHome
+      let homeTeamWon = this.state.game.home.runs > this.state.game.away.runs
+      if ((myTeamAtHome && homeTeamWon) || (!myTeamAtHome && !homeTeamWon)){
+        body = (
+          <div>
+            <ul className="ScoresList">
+              <li>
+                <p> You know it! </p>
+                <p> {this.state.game.home.name} - {this.state.game.home.runs} </p>
+                <p> {this.state.game.away.name} - {this.state.game.away.runs} </p>
+              </li>
+            </ul>
+          </div>
         )
       }
-    })
+      else {
+        body = (
+          <div>
+            <ul className="ScoresList">
+              <li>
+                <p> Not Today Champ </p>
+                <p> {this.state.game.home.name} - {this.state.game.home.runs} </p>
+                <p> {this.state.game.away.name} - {this.state.game.away.runs} </p>
+              </li>
+            </ul>
+          </div>
+          )
+      }
+    }
 
-    if(this.state.offday === true){
-      return(
-        <div>
-        < p className="noGame"> No game so far today! Check back later</p>
-        </div>
-    )
-  }
-  else {
     return (
       <div className="WinButton">
-        {gameinfo}
+        {body}
+        <Schedule today = {this.state.today} teamId = {this.state.teamId}/>
       </div>
     )
-   }
   }
-
 
 }
 
 export default TodaysScore
+
+
+
+
