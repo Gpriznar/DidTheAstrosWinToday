592c2ba8d3db465244b5c787c6fae403a99396bc
Gregory Priznar <44954435+Gpriznar@users.noreply.github.com>
2019-07-12 13:08:53 -0500

    Added two components that will randomize victory or defeat messages

---
+++ didtheastroswintoday/src/components/DefeatMessage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+export class DefeatMessage extends Component {
+
+render() {
+  return(
+    <div>
+    <p> Randomized Defeat Message </p>
+    </div>
+  )
+}
+
+
+}
+
+export default DefeatMessage
