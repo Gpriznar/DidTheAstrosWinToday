48f1408212b0976b96d9f91e062422f3e0c18911
Tim Goens <timcgoens@gmail.com>
2019-07-26 09:53:23 -0500

    final, FINAL update

--- didtheastroswintoday/build/precache-manifest.c1091da045099a8090515267cfb5b4fa.js
+++ didtheastroswintoday/build/precache-manifest.c1091da045099a8090515267cfb5b4fa.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f30b3dd2d7ed9e79895c65dcc6befd9b",
+    "revision": "d51a8643aefe8c3366f4fd28db7447ca",
     "url": "/index.html"
   },
   {
-    "revision": "2779260c2eb3ce855822",
-    "url": "/static/css/main.baed7d1e.chunk.css"
+    "revision": "de0b141d2066858796b5",
+    "url": "/static/css/main.f4ed03dd.chunk.css"
   },
   {
     "revision": "0db44657c5cac826ce8e",
     "url": "/static/js/2.a62b3b02.chunk.js"
   },
   {
-    "revision": "2779260c2eb3ce855822",
-    "url": "/static/js/main.ffb89901.chunk.js"
+    "revision": "de0b141d2066858796b5",
+    "url": "/static/js/main.9a06071c.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
