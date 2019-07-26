da316f0c413d4ef849f7e2417b620409393d005c
Tim Goens <timcgoens@gmail.com>
2019-07-26 08:51:14 -0500

    final version 1 update

--- didtheastroswintoday/src/components/DefeatMessage.js
+++ didtheastroswintoday/src/components/DefeatMessage.js
@@ -4,7 +4,7 @@ export class DefeatMessage extends Component {
 
 render() {
 
-  let defeatMessages = ['Uh oh', 'Maybe next time', 'Not today', 'Dang...', 'Unfortunately Not', 'NOOOOOO!',]
+  let defeatMessages = ['Uh oh', 'Maybe next time', 'Not today', 'Dang...', 'Unfortunately Not', 'NOOOOOO!', 'Baseball is bad right now']
   let randomIndex = Math.floor(Math.random() * defeatMessages.length)
   let randomDefeatMessage = defeatMessages[randomIndex]
 
--- didtheastroswintoday/src/components/VictoryMessage.js
+++ didtheastroswintoday/src/components/VictoryMessage.js
@@ -4,7 +4,7 @@ export class VictoryMessage extends Component {
 
 render() {
 
-let winMessages = ['Oh yea!', 'You know it!', 'What else did you expect?', 'Yup!', 'Woohoo!']
+let winMessages = ['Oh yea!', 'You know it!', 'What else did you expect?', 'Yup!', 'Woooooooo!', 'TOOT TOOT!', 'Baseball it good right now!']
 let randomIndex = Math.floor(Math.random() * winMessages.length)
 let randomVictoryMessage = winMessages[randomIndex]
 
